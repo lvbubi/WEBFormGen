cfb82a220866d3c66ce8f824583eecfd1bf5ed01
Bence Bubori <lvbubi2@gmail.com>
2017-04-17 18:02:03 +0200

    Updated UserBean, User.xhtml
    Added dynamic selection lists (from EJB)
    Other added other option for a list (hidden div)
    Most of the stuffs are prepared

--- UtazasTervezes/src/java/Beans/UserBean.java
+++ UtazasTervezes/src/java/Beans/UserBean.java
@@ -12,6 +12,7 @@ package Beans;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
@@ -32,6 +33,47 @@ import webformgen.ProcedureManager;
 @ManagedBean
 public class UserBean {
     String neptun,password;
+    String honnan,hova;
+    String rendszam,Eloadas_postercim,egyebkeret;
+
+    public String getEgyebkeret() {
+        return egyebkeret;
+    }
+
+    public void setEgyebkeret(String egyebkeret) {
+        this.egyebkeret = egyebkeret;
+    }
+
+    public String getRendszam() {
+        return rendszam;
+    }
+
+    public void setRendszam(String rendszam) {
+        this.rendszam = rendszam;
+    }
+    
+    public String getEloadas_postercim() {
+        return Eloadas_postercim;
+    }
+
+    public void setEloadas_postercim(String Eloadas_postercim) {
+        this.Eloadas_postercim = Eloadas_postercim;
+    }
+    public String getHonnan() {
+        return honnan;
+    }
+
+    public void setHonnan(String honnan) {
+        this.honnan = honnan;
+    }
+
+    public String getHova() {
+        return hova;
+    }
+
+    public void setHova(String hova) {
+        this.hova = hova;
+    }
     int PersonID=-1;
     @EJB
     private ProcedureManager SingletonDBMgr;
@@ -58,16 +100,35 @@ public class UserBean {
         webformgen.Person p = SingletonDBMgr.getPersonDatas(PersonID);
         return p.getVnev()+" "+p.getKnev();
     }
-
-    
-    public String getPrice(){
-        try{
+    public List<String> getRendszamok(){
+        return SingletonDBMgr.getRendszamok(PersonID);
+    }
+    public List<String> getUtazasiKeret(){
+        List<String> tmp=new ArrayList<>();
+        tmp.add("TEMPUS");
+        tmp.add("egyéb EU - szintű együttműködés");
+        tmp.add("kormányközi együttműködés");
+        tmp.add("intézményi együttműködés");
+        tmp.add("alapítványi támogatás");
+        tmp.add("meghívás");
+        tmp.add("saját szervezés");
+        tmp.add("egyéb:");
+        return tmp;
+    }
+    public int getPrice(){
+        /*try{
             DWNLDATA ddata=new DWNLDATA();
-            List<String> uzemanyag=ddata.getFuel();
-            return uzemanyag.get(0);
+            return ddata.selectUzemanyag("uzemanyagtipus", "honap");
+           
         }
         catch(IOException ex){
-            return "null";
-        }
+            return -1;
+        }*/
+        return 0;
+    }
+    
+    public void SelectedRendszam(){
+        System.out.println(rendszam);
+        System.out.println("MIAFASZVAN");
     }
 }
